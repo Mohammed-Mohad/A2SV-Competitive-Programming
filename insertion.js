db12e66730f93ff7c3ad16c91a2fda3750f99904
Mohammed-Mohad <mohamed.mehad@aastustudent.edu.et>
2023-02-10 22:09:47 +0300

    inserton

---
+++ insertion.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+function insertionSort1(n, arr) {
+    // Write your code here
+    let key = arr[n-1];
+    let i = n-1;
+    while (i > 0 && arr[i - 1] > key) {
+        arr[i] = arr[i - 1];
+        console.log(arr.join(" "))
+        i--;
+    }
+    arr[i] = key;
+    console.log(arr.join(" "));
+
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    insertionSort1(n, arr);
+}
